0751be83060adb1c381d91c003d8cf5b574ca9e1
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-04-25 01:15:03 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,45 +1,36 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- * order using the Insertion sort algorithm.
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
  *
- * @list: Pointer to the head of the list to be sorted
- *
- * Description: You are not allowed to modify the integer n of a node. You have
- * to swap the nodes themselves.
- *
- * Return: void
+ * @list: Pointer to the head of the doubly linked list
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev_node, *next_node;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    listint_t *curr, *tmp;
 
-	current = (*list)->next;
-	while (current != NULL)
-	{
-		prev_node = current->prev;
-		next_node = current->next;
+    for (curr = (*list)->next; curr != NULL; curr = curr->next)
+    {
+        while (curr->prev != NULL && curr->prev->n > curr->n)
+        {
+            tmp = curr->prev;
+            curr->prev = tmp->prev;
 
-		while (prev_node != NULL && prev_node->n > current->n)
-		{
-			if (prev_node->prev != NULL)
-				prev_node->prev->next = current;
-			else
-				*list = current;
+            if (tmp->prev != NULL)
+                tmp->prev->next = curr;
+            else
+                *list = curr;
 
-			current->prev = prev_node->prev;
-			prev_node->next = next_node;
-			prev_node->prev = current;
-			next_node->prev = prev_node;
+            tmp->next = curr->next;
+            if (curr->next != NULL)
+                curr->next->prev = tmp;
+            curr->next = tmp;
+            tmp->prev = curr;
 
-			prev_node = current->prev;
-			print_list(*list);
-		}
-		current = next_node;
-	}
+            print_list(*list);
+        }
+    }
 }
