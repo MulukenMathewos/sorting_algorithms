7ff2143cabbd0fc3bcbcf3f6c4ff66db18c55489
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-04-25 00:47:23 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -33,5 +33,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap(int *a, int *b);
+void quicksort_recursive(int *array, int start, int end, size_t size);
 
 #endif
