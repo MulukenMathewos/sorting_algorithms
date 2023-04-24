e3917e835eba0349f3e766aa56bf58fb44323a08
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-04-25 01:04:38 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -35,5 +35,6 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void swap(int *a, int *b);
 void quicksort_recursive(int *array, int start, int end, size_t size);
+int lomuto_partition(int *array, int start, int end, size_t size);
 
 #endif
