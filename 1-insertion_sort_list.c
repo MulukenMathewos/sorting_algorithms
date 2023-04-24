d1b8e7a9bcd7b34683b50bf1f4534ee32aab9ce0
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-24 14:35:23 -0700

    lol

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,39 +1,45 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in
- * ascending order using the Insertion sort algorithm
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm.
  *
- * @list: pointer to head of list
+ * @list: Pointer to the head of the list to be sorted
+ *
+ * Description: You are not allowed to modify the integer n of a node. You have
+ * to swap the nodes themselves.
+ *
+ * Return: void
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
+	listint_t *current, *prev_node, *next_node;
 
-	current = (*list)->next;
-	if (!list || !(*list) || !((*list)->next))
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	while (current)
+	current = (*list)->next;
+	while (current != NULL)
 	{
-		listint_t *temp = current->prev;
+		prev_node = current->prev;
+		next_node = current->next;
 
-		while (temp && temp->n > current->n)
+		while (prev_node != NULL && prev_node->n > current->n)
 		{
-			if (temp->prev)
-				temp->prev->next = current;
+			if (prev_node->prev != NULL)
+				prev_node->prev->next = current;
 			else
-				(*list) = current;
+				*list = current;
 
-			current->prev = temp->prev;
-			temp->prev = current;
-			temp->next = current->next;
-			current->next = temp;
+			current->prev = prev_node->prev;
+			prev_node->next = next_node;
+			prev_node->prev = current;
+			next_node->prev = prev_node;
 
+			prev_node = current->prev;
 			print_list(*list);
-			temp = current->prev;
 		}
-
-		current = current->next;
+		current = next_node;
 	}
 }
