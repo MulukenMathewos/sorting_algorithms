64dee1f638b2f00a3c96faed34791ec0fd2ed79e
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-24 15:32:02 -0700

    lol

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,28 +11,28 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int tmp;
+	size_t gap = 1, i, j;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (gap < size)
-        gap = gap * 3 + 1;
+	while (gap < size)
+		gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            tmp = array[i];
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
 
-            for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
-                array[j] = array[j - gap];
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
 
-            array[j] = tmp;
-        }
+			array[j] = tmp;
+		}
 
-        gap = (gap - 1) / 3;
-        print_array(array, size);
-    }
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
 }
