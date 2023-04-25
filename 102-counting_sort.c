f4c3c73a3a9e93816c39a0aba3d88912024eead5
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-25 01:41:57 -0700

    love.

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,40 +11,38 @@
  */
 void counting_sort(int *array, size_t size)
 {
-    int *count_arr;
-    int max_num, i, j;
+	int *count_arr;
+	int max_num, i, j;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    max_num = array[0];
-    for (i = 1; i < (int)size; i++)
-    {
-        if (array[i] > max_num)
-            max_num = array[i];
-    }
+	max_num = array[0];
+	for (i = 1; i < (int)size; i++)
+	{
+		if (array[i] > max_num)
+			max_num = array[i];
+	}
 
-    count_arr = malloc(sizeof(int) * (max_num + 1));
-    if (!count_arr)
-        return;
+	count_arr = malloc(sizeof(int) * (max_num + 1));
+	if (!count_arr)
+		return;
 
-    for (i = 0; i <= max_num; i++)
-        count_arr[i] = 0;
+	for (i = 0; i <= max_num; i++)
+		count_arr[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count_arr[array[i]]++;
 
-    for (i = 0; i < (int)size; i++)
-        count_arr[array[i]]++;
+	printf("%d", count_arr[0]);
+	for (i = 1; i <= max_num; i++)
+		printf(", %d", count_arr[i]);
+	printf("\n");
 
-    printf("%d", count_arr[0]);
-    for (i = 1; i <= max_num; i++)
-        printf(", %d", count_arr[i]);
-    printf("\n");
-
-    j = 0;
-    for (i = 0; i <= max_num; i++)
-    {
-        while (count_arr[i]--)
-            array[j++] = i;
-    }
-
-    free(count_arr);
+	j = 0;
+	for (i = 0; i <= max_num; i++)
+	{
+		while (count_arr[i]--)
+			array[j++] = i;
+	}
+	free(count_arr);
 }
