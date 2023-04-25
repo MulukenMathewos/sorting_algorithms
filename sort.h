95e2ee10d5833d717ebdc84abf98d90ce42b5da9
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-04-25 12:48:03 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -28,6 +28,8 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t **a, listint_t **b, listint_t **list);
+void swap(int *array, int i, int j);
+void sift_down(int *array, int start, int end, size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge(int *array, int left, int mid, int right);
