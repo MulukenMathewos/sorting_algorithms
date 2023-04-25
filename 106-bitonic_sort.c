585bfa0addfe98f9a682a5e0730ba3f814a06ac5
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-25 08:44:44 -0700

    lop

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -12,41 +12,45 @@
  */
 void bitonic_merge(int *array, size_t start, size_t count, bool direction)
 {
-    if (count > 1)
-    {
-        size_t k = count / 2;
-        for (size_t i = start; i < start + k; i++)
-        {
-            if ((array[i] > array[i + k]) == direction)
-            {
-                int temp = array[i];
-                array[i] = array[i + k];
-                array[i + k] = temp;
-                printf("Merging [%lu/%lu] (%s):\n", count, count, direction ? "UP" : "DOWN");
-                print_array(array, start + count);
-            }
-        }
-        bitonic_merge(array, start, k, direction);
-        bitonic_merge(array, start + k, k, direction);
-    }
+	if (count > 1)
+	{
+		size_t k = count / 2;
+
+		for (size_t i = start; i < start + k; i++)
+		{
+			if ((array[i] > array[i + k]) == direction)
+			{
+				int temp = array[i];
+
+				array[i] = array[i + k];
+				array[i + k] = temp;
+				printf("Merging [%lu/%lu] (%s):\n", count, count, direction ? "UP" : "DOWN");
+				print_array(array, start + count);
+			}
+		}
+		bitonic_merge(array, start, k, direction);
+		bitonic_merge(array, start + k, k, direction);
+	}
 }
 
 /**
- * bitonic_sort - Sort an array of integers in ascending order using the bitonic sort algorithm
+ * bitonic_sort - Sort an array of integers in ascending order
+ * using the bitonic sort algorithm
  * @array: Array to sort
  * @size: Size of the array
  */
 void bitonic_sort(int *array, size_t size)
 {
-    for (size_t k = 2; k <= size; k *= 2)
-    {
-        for (size_t j = k / 2; j > 0; j /= 2)
-        {
-            for (size_t i = 0; i < size; i += k)
-            {
-                bool direction = ((i / j) % 2) == 0;
-                bitonic_merge(array, i, k, direction);
-            }
-        }
-    }
+	for (size_t k = 2; k <= size; k *= 2)
+	{
+		for (size_t j = k / 2; j > 0; j /= 2)
+		{
+			for (size_t i = 0; i < size; i += k)
+			{
+				bool direction = ((i / j) % 2) == 0;
+
+				bitonic_merge(array, i, k, direction);
+			}
+		}
+	}
 }
