246c4126449606750aea8734c522fd99aeecd792
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-25 09:37:10 -0700

    lopo

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -8,21 +8,27 @@
  * @size: Number of elements to merge
  * @ascending: True for ascending order, false for descending
  */
-void bitonic_merge(int *array, size_t size, int ascending) {
-    if (size > 1) {
-        size_t i, mid = size / 2;
-        bitonic_merge(array, mid, 1);
-        bitonic_merge(array + mid, mid, 0);
-        for (i = 0; i < mid; i++) {
-            if ((array[i] > array[i + mid]) == ascending) {
-                int temp = array[i];
-                array[i] = array[i + mid];
-                array[i + mid] = temp;
-            }
-        }
-        printf("Merging [%zu/%zu] (%s):\n", size, size, ascending ? "UP" : "DOWN");
-        print_array(array, size);
-    }
+void bitonic_merge(int *array, size_t size, int ascending)
+{
+	if (size > 1)
+	{
+		size_t i, mid = size / 2;
+
+		bitonic_merge(array, mid, 1);
+		bitonic_merge(array + mid, mid, 0);
+		for (i = 0; i < mid; i++)
+		{
+			if ((array[i] > array[i + mid]) == ascending)
+			{
+				int temp = array[i];
+
+				array[i] = array[i + mid];
+				array[i + mid] = temp;
+			}
+		}
+		printf("Merging [%lu/%lu] (%s):\n", size, size, ascending ? "UP" : "DOWN");
+		print_array(array, size);
+	}
 }
 
 /**
@@ -31,13 +37,16 @@ void bitonic_merge(int *array, size_t size, int ascending) {
  * @array: Array to sort
  * @size: Size of the array
  */
-void bitonic_sort(int *array, size_t size) {
-    if (size > 1) {
-        size_t i, mid = size / 2;
-        bitonic_sort(array, mid);
-        bitonic_sort(array + mid, mid);
-        bitonic_merge(array, size, 1);
-        printf("Merging [%zu/%zu] (UP):\n", size, size);
-        print_array(array, size);
-    }
+void bitonic_sort(int *array, size_t size)
+{
+	if (size > 1)
+	{
+		size_t mid = size / 2;
+
+		bitonic_sort(array, mid);
+		bitonic_sort(array + mid, mid);
+		bitonic_merge(array, size, 1);
+		printf("Merging [%lu/%lu] (UP):\n", size, size);
+		print_array(array, size);
+	}
 }
