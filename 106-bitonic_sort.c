0fb0bcd66ef9e1e29ff82c4914292164c72a71ea
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-04-25 19:21:42 +0300

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -5,32 +5,24 @@
 /**
  * bitonic_merge - Merge two bitonic sequences in ascending or descending order
  * @array: Array to sort
- * @start: Starting index of the sequence to merge
- * @count: Number of elements to merge
- * @direction: True for ascending order, false for descending
+ * @size: Number of elements to merge
+ * @ascending: True for ascending order, false for descending
  */
-void bitonic_merge(int *array, size_t start, size_t count, bool direction)
-{
-	if (count > 1)
-	{
-		size_t k = count / 2;
-
-		for (size_t i = start; i < start + k; i++)
-		{
-			if ((array[i] > array[i + k]) == direction)
-			{
-				int temp = array[i];
-
-				array[i] = array[i + k];
-				array[i + k] = temp;
-				printf("Merging [%lu/%lu] (%s):\n", count, count,
-						direction ? "UP" : "DOWN");
-				print_array(array, start + count);
-			}
-		}
-		bitonic_merge(array, start, k, direction);
-		bitonic_merge(array, start + k, k, direction);
-	}
+void bitonic_merge(int *array, size_t size, int ascending) {
+    if (size > 1) {
+        size_t i, mid = size / 2;
+        bitonic_merge(array, mid, 1);
+        bitonic_merge(array + mid, mid, 0);
+        for (i = 0; i < mid; i++) {
+            if ((array[i] > array[i + mid]) == ascending) {
+                int temp = array[i];
+                array[i] = array[i + mid];
+                array[i + mid] = temp;
+            }
+        }
+        printf("Merging [%zu/%zu] (%s):\n", size, size, ascending ? "UP" : "DOWN");
+        print_array(array, size);
+    }
 }
 
 /**
@@ -39,18 +31,13 @@ void bitonic_merge(int *array, size_t start, size_t count, bool direction)
  * @array: Array to sort
  * @size: Size of the array
  */
-void bitonic_sort(int *array, size_t size)
-{
-	for (size_t k = 2; k <= size; k *= 2)
-	{
-		for (size_t j = k / 2; j > 0; j /= 2)
-		{
-			for (size_t i = 0; i < size; i += k)
-			{
-				bool direction = ((i / j) % 2) == 0;
-
-				bitonic_merge(array, i, k, direction);
-			}
-		}
-	}
+void bitonic_sort(int *array, size_t size) {
+    if (size > 1) {
+        size_t i, mid = size / 2;
+        bitonic_sort(array, mid);
+        bitonic_sort(array + mid, mid);
+        bitonic_merge(array, size, 1);
+        printf("Merging [%zu/%zu] (UP):\n", size, size);
+        print_array(array, size);
+    }
 }
