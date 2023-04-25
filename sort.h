08e5e582e3fa66703363087b15df40dc7fd1a40d
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-04-25 13:00:56 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -28,7 +28,7 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t **a, listint_t **b, listint_t **list);
-void swap(int *array, int i, int j);
+void swap(int *a, int *b);
 void sift_down(int *array, int start, int end, size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
@@ -37,7 +37,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void swap(int *a, int *b);
 void quicksort_recursive(int *array, int start, int end, size_t size);
 int lomuto_partition(int *array, int start, int end, size_t size);
 
