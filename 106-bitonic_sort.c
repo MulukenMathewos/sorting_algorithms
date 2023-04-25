f5b8549fc6ca4183be55e34c31e7227ca2388688
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-25 10:09:02 -0700

    Love you.

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -8,52 +8,50 @@
  */
 void bitonic_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    bitonic_recursive(array, size, 0, size, 1);
+	bitonic_re(array, size, 0, size, 1);
 }
 
 /**
- * bitonic_recursive - Recursively sorts an array of integers in ascending order
- *                     using the Bitonic sort algorithm
+ * bitonic_re - Recursively sorts an array of integers in
+ * ascending order using the Bitonic sort algorithm
  * @array: The array to be sorted
  * @size: The size of the array
  * @start: The starting index of the subarray to be sorted
  * @count: The number of elements in the subarray to be sorted
  * @order: The order of sorting (1 for ascending, -1 for descending)
  */
-void bitonic_recursive(int *array, size_t size, size_t start, size_t count, int order)
+void bitonic_re(int *array, size_t size, size_t start, size_t count, int order)
 {
-    size_t half = count / 2;
-    int swapped = 0;
+	size_t i, half = count / 2;
+	int swapped = 0;
 
-    if (count > 1)
-    {
-        printf("Merging [%lu/%lu] (%s):\n", count, size, order == 1 ? "UP" : "DOWN");
-        print_array(array + start, count);
+	if (count > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", count, size, order == 1 ? "UP" : "DOWN");
+		print_array(array + start, count);
 
-        for (size_t i = start; i < start + half; i++)
-        {
-            if ((array[i] > array[i + half]) == order)
-            {
-                swap(&array[i], &array[i + half]);
-                swapped = 1;
-            }
-        }
-
-        if (swapped)
-        {
-            bitonic_recursive(array, size, start, half, order);
-            bitonic_recursive(array, size, start + half, half, order);
-        }
-    }
-
-    if (count == size)
-    {
-        printf("Result [%lu/%lu] (%s):\n", count, size, order == 1 ? "UP" : "DOWN");
-        print_array(array + start, count);
-    }
+		for (i = start; i < start + half; i++)
+		{
+			if ((array[i] > array[i + half]) == order)
+			{
+				swap(&array[i], &array[i + half]);
+				swapped = 1;
+			}
+		}
+		if (swapped)
+		{
+			bitonic_re(array, size, start, half, order);
+			bitonic_re(array, size, start + half, half, order);
+		}
+	}
+	if (count == size)
+	{
+		printf("Result [%lu/%lu] (%s):\n", count, size, order == 1 ? "UP" : "DOWN");
+		print_array(array + start, count);
+	}
 }
 
 /**
@@ -63,10 +61,10 @@ void bitonic_recursive(int *array, size_t size, size_t start, size_t count, int 
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 
-    printf("Result [%d/%d] (UP):\n", *a, *b);
-    print_array((const int *)a, 2);
+	printf("Result [%d/%d] (UP):\n", *a, *b);
+	print_array((const int *)a, 2);
 }
--- sort.h
+++ sort.h
@@ -37,7 +37,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void counting_sort1(int *array, size_t size, int exp);
 void bitonic_sort(int *array, size_t size);
-void bitonic_recursive(int *array, size_t size, size_t start, size_t count, int order);
+void bitonic_re(int *array, size_t size, size_t start, size_t count, int order);
 void quick_sort_hoare(int *array, size_t size);
 void quicksort_recursive(int *array, int start, int end, size_t size);
 int lomuto_partition(int *array, int start, int end, size_t size);
