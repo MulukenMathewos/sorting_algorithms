dac3eb541e067c2e9726de442039ddfdd2c08ed6
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-25 11:57:24 -0700

    lop

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,48 +1,63 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 #include "deck.h"
 
-/* function to compare two cards */
+/**
+ * card_cmp - compares two cards
+ *
+ * @c1: pointer to first card
+ * @c2: pointer to second card
+ *
+ * Return: negative value if c1 < c2, positive value if c1 > c2, zero if equal
+ */
 int card_cmp(const void *c1, const void *c2)
 {
-    const card_t *card1 = ((const deck_node_t *)c1)->card;
-    const card_t *card2 = ((const deck_node_t *)c2)->card;
-    int val_cmp = strcmp(card1->value, card2->value);
-    if (val_cmp != 0)
-        return val_cmp;
-    return card1->kind - card2->kind;
+	const card_t *card1 = ((const deck_node_t *)c1)->card;
+	const card_t *card2 = ((const deck_node_t *)c2)->card;
+	int val_cmp = strcmp(card1->value, card2->value);
+
+	if (val_cmp != 0)
+		return (val_cmp);
+	return (card1->kind - card2->kind);
 }
 
-/* function to sort a deck of cards */
+/**
+ * sort_deck - sorts a deck of cards
+ *
+ * @deck: pointer to pointer to head of doubly-linked list of cards
+ */
 void sort_deck(deck_node_t **deck)
 {
-    if (deck == NULL || *deck == NULL)
-        return;
-    size_t len = 0;
-    deck_node_t *curr = *deck;
-    while (curr != NULL)
-    {
-        len++;
-        curr = curr->next;
-    }
-    /* convert doubly-linked list to array */
-    deck_node_t **arr = malloc(len * sizeof(deck_node_t *));
-    curr = *deck;
-    for (size_t i = 0; i < len; i++)
-    {
-        arr[i] = curr;
-        curr = curr->next;
-    }
-    /* sort the array */
-    qsort(arr, len, sizeof(deck_node_t *), card_cmp);
-    /* convert array back to doubly-linked list */
-    *deck = arr[0];
-    (*deck)->prev = NULL;
-    for (size_t i = 1; i < len; i++)
-    {
-        arr[i]->prev = arr[i - 1];
-        arr[i - 1]->next = arr[i];
-    }
-    arr[len - 1]->next = NULL;
-    free(arr);
+	size_t i, len = 0;
+	deck_node_t **arr, *curr;
+
+	if (deck == NULL || *deck == NULL)
+		return;
+
+	while (curr != NULL)
+	{
+		len++;
+		curr = curr->next;
+	}
+	
+	curr = *deck;
+	arr = malloc(len * sizeof(deck_node_t *));
+	for (i = 0; i < len; i++)
+	{
+		arr[i] = curr;
+		curr = curr->next;
+	}
+	/* sort the array */
+	qsort(arr, len, sizeof(deck_node_t *), card_cmp);
+	/* convert array back to doubly-linked list */
+	*deck = arr[0];
+	(*deck)->prev = NULL;
+	for (i = 1; i < len; i++)
+	{
+		arr[i]->prev = arr[i - 1];
+		arr[i - 1]->next = arr[i];
+	}
+	arr[len - 1]->next = NULL;
+	free(arr);
 }
--- deck.h
+++ deck.h
@@ -1,12 +1,23 @@
 #ifndef DECK_H
 #define DECK_H
 
+/**
+ * enum kind_e - Represents the four kinds of a deck of cards
+ * @SPADE: Represents the SPADE kind
+ * @HEART: Represents the HEART kind
+ * @CLUB: Represents the CLUB kind
+ * @DIAMOND: Represents the DIAMOND kind
+ * Description: This enum type defines the four kinds of a deck of cards,
+ * namely spades, hearts, clubs, and diamonds. Each of these kinds
+ * are assigned a unique integer value starting from 0 for SPADE,
+ * 1 for HEART, 2 for CLUB, and 3 for DIAMOND.
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -18,8 +29,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -31,11 +42,12 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
+int card_cmp(const void *c1, const void *c2);
 
 #endif /* DECK_H */
