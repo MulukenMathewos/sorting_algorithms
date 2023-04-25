87eea8cbf6a5faed7729ddbac7b66875352c117c
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-25 04:01:50 -0700

    lolk

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -11,14 +11,15 @@
  */
 void swap(int *array, int i, int j)
 {
-    int temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+	int temp = array[i];
+
+	array[i] = array[j];
+	array[j] = temp;
 }
 
 /**
- * sift_down - Moves an element down the heap until it is in the correct position
- *
+ * sift_down - Moves an element down the heap until
+ * it is in the correct position
  * @array: The array containing the heap
  * @start: The index of the element to move down the heap
  * @end: The index of the last element in the heap
@@ -28,27 +29,25 @@ void swap(int *array, int i, int j)
  */
 void sift_down(int *array, int start, int end, size_t size)
 {
-    int root = start;
+	int root = start;
 
-    while ((root * 2) + 1 <= end)
-    {
-        int child = (root * 2) + 1;
-        int swap_idx = root;
+	while ((root * 2) + 1 <= end)
+	{
+		int child = (root * 2) + 1;
+		int swap_idx = root;
 
-        if (array[swap_idx] < array[child])
-            swap_idx = child;
+		if (array[swap_idx] < array[child])
+			swap_idx = child;
+		if (child + 1 <= end && array[swap_idx] < array[child + 1])
+			swap_idx = child + 1;
+		if (swap_idx == root)
+			return;
 
-        if (child + 1 <= end && array[swap_idx] < array[child + 1])
-            swap_idx = child + 1;
+		swap(array, root, swap_idx);
+		print_array(array, size);
 
-        if (swap_idx == root)
-            return;
-
-        swap(array, root, swap_idx);
-        print_array(array, size);
-
-        root = swap_idx;
-    }
+		root = swap_idx;
+	}
 }
 
 /**
@@ -62,16 +61,16 @@ void sift_down(int *array, int start, int end, size_t size)
  */
 void heap_sort(int *array, size_t size)
 {
-    int end = size - 1;
+	int end = size - 1;
 
-    for (int start = (end - 1) / 2; start >= 0; start--)
-        sift_down(array, start, end, size);
+	for (int start = (end - 1) / 2; start >= 0; start--)
+		sift_down(array, start, end, size);
 
-    while (end > 0)
-    {
-        swap(array, 0, end);
-        print_array(array, size);
-        end--;
-        sift_down(array, 0, end, size);
-    }
+	while (end > 0)
+	{
+		swap(array, 0, end);
+		print_array(array, size);
+		end--;
+		sift_down(array, 0, end, size);
+	}
 }
