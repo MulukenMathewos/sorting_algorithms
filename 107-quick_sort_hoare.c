302c4646b67f6d339689250f5aa2ea98950a5dd2
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-04-25 11:15:13 -0700

    Let me know.

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -7,9 +7,10 @@
  */
 void swap(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -22,26 +23,25 @@ void swap(int *a, int *b)
  */
 int hoare_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1;
-    int j = high + 1;
+	int pivot = array[high];
+	int i = low - 1;
+	int j = high + 1;
 
-    while (1)
-    {
-        do {
-            i++;
-        } while (array[i] < pivot);
+	while (1)
+	{
+		do {
+			i++;
+		} while (array[i] < pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
 
-        do {
-            j--;
-        } while (array[j] > pivot);
+		if (i >= j)
+			return (j);
 
-        if (i >= j)
-            return (j);
-
-        swap(&array[i], &array[j]);
-        print_array(array, size);
-    }
+		swap(&array[i], &array[j]);
+		print_array(array, size);
+	}
 }
 
 /**
@@ -53,25 +53,26 @@ int hoare_partition(int *array, int low, int high, size_t size)
  */
 void quicksort_hoare(int *array, int low, int high, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (low < high)
-    {
-        pivot = hoare_partition(array, low, high, size);
-        quicksort_hoare(array, low, pivot, size);
-        quicksort_hoare(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot = hoare_partition(array, low, high, size);
+		quicksort_hoare(array, low, pivot, size);
+		quicksort_hoare(array, pivot + 1, high, size);
+	}
 }
 
 /**
- * quick_sort_hoare - sorts array using quicksort algorithm with hoare partition scheme
+ * quick_sort_hoare - sorts array using quicksort algorithm
+ * with hoare partition scheme
  * @array: array to sort
  * @size: size of the array
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-    if (array && size > 1)
-    {
-        quicksort_hoare(array, 0, size - 1, size);
-    }
+	if (array && size > 1)
+	{
+		quicksort_hoare(array, 0, size - 1, size);
+	}
 }
